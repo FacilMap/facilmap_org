9d2e95daf0c1116eeff0959a761603da100b3101
Candid Dauth <cdauth@cdauth.eu>
2010-11-30 00:48:21 +0000

    Fixed issue with Query String decoding.

--- map.js
+++ map.js
@@ -30,7 +30,7 @@ function initMap()
 	if(location.search.length > 1)
 	{ // Move query string to location hash part
 		var search_obj = FacilMap.Util.decodeQueryString(location.search.substr(1));
-		var hash_obj = FacilMap.Util.decodeQueryString(OpenLayers.URLHashHandler.prototype.getLocationHash());
+		var hash_obj = FacilMap.Util.decodeQueryString(FacilMap.URLHashHandler.prototype.getLocationHash());
 		for(var i in search_obj)
 			hash_obj[i] = search_obj[i];
 		location.replace(location.pathname+"#"+FacilMap.Util.encodeQueryString(hash_obj));
